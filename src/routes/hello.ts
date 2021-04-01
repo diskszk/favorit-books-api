3c6119483c35eba79e9bea257aec55f7d2f9a400
diskszk <49810133+diskszk@users.noreply.github.com>
2021-04-01 18:32:55 +0900

    add: add database settings (#7)

--- src/routes/hello.ts
+++ src/routes/hello.ts
@@ -6,9 +6,7 @@ export const helloRouter = Router();
 helloRouter.get(
   '/api/hello',
   wrap(async (req, res) => {
-    setTimeout(() => {
-      res.status(200);
-      res.json({ hoge: 'apple' });
-    }, 1000);
+    res.status(200);
+    res.json({ hoge: 'apple' });
   })
 );
